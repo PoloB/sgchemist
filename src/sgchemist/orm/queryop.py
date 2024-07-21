371a3922d7b8e3f26c07ebc8e26d3d800c11ccf0
PoloB <paulemilebuteau@gmail.com>
2024-07-22 00:02:17 +0200

    add first implementation for field matching

--- src/sgchemist/orm/queryop.py
+++ src/sgchemist/orm/queryop.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import abc
+import datetime
 from typing import TYPE_CHECKING
 from typing import Any
 from typing import TypeVar
@@ -13,6 +14,7 @@ from .constant import LogicalOperator
 from .constant import Operator
 
 if TYPE_CHECKING:
+    from . import SgBaseEntity
     from .fields import AbstractField
 
 T = TypeVar("T")
@@ -45,6 +47,10 @@ class SgFilterObject(object):
             combined object.
         """
 
+    @abc.abstractmethod
+    def matches(self, entity: SgBaseEntity) -> bool:
+        """Return True if this filter matches the given entity."""
+
 
 class SgNullCondition(SgFilterObject):
     """Defines a null condition."""
@@ -70,6 +76,10 @@ class SgNullCondition(SgFilterObject):
             combined object.
         """
         return other
+
+    def matches(self, entity: SgBaseEntity) -> bool:
+        """A null condition always matches the given entity."""
+        return True
 
 
 class SgFilterOperation(SgFilterObject):
@@ -128,6 +138,14 @@ class SgFilterOperation(SgFilterObject):
         """
         return self._op(LogicalOperator.ANY, other)
 
+    def matches(self, entity: SgBaseEntity) -> bool:
+        """Return True if the entity matches the given filter."""
+        test_func = {
+            LogicalOperator.ALL: all,
+            LogicalOperator.ANY: any,
+        }[self.operator]
+        return test_func(sg_object.matches(entity) for sg_object in self.sg_objects)
+
 
 class SgFieldCondition(SgFilterObject):
     """Defines a field condition."""
@@ -164,3 +182,73 @@ class SgFieldCondition(SgFilterObject):
             combined object.
         """
         return SgFilterOperation(LogicalOperator.ANY, [self, other])
+
+    def matches(self, entity: SgBaseEntity) -> bool:
+        """Return True if the given entity matches the filter."""
+        value = entity.get_value(self.field)
+        if self.operator == Operator.BETWEEN:
+            # TODO: check if the shotgrid implementation is strict or not
+            return self.right[0] >= value >= self.right[1]
+        elif self.operator == Operator.CONTAINS:
+            return self.right in value
+        elif self.operator == Operator.ENDS_WITH:
+            return value.endswith(self.right)
+        elif self.operator == Operator.GREATER_THAN:
+            # TODO: check if the shotgrid implementation is strict or not
+            return value >= self.right
+        elif self.operator == Operator.IN:
+            return value in self.right
+        # TODO: check if the shotgrid implementation is strict or not
+        elif self.operator == Operator.IN_CALENDAR_DAY:
+            return value - datetime.date.today() <= datetime.timedelta(days=self.right)
+        # TODO: check if the shotgrid implementation is strict or not
+        elif self.operator == Operator.IN_CALENDAR_MONTH:
+            # There are not always the same number of days between two months
+            today = datetime.date.today()
+            calendar_month = today.replace(month=today.month + self.right)
+            return value <= calendar_month
+        # TODO: check if the shotgrid implementation is strict or not
+        elif self.operator == Operator.IN_CALENDAR_WEEK:
+            return value - datetime.date.today() <= datetime.timedelta(weeks=self.right)
+        # TODO: check if the shotgrid implementation is strict or not
+        elif self.operator == Operator.IN_CALENDAR_YEAR:
+            # There are not always the same number of days between two years
+            today = datetime.date.today()
+            calendar_year = today.replace(year=today.year + self.right)
+            return value <= calendar_year
+        # TODO: check if the shotgrid implementation is strict or not
+        elif self.operator == Operator.IN_LAST:
+            time_value = {f"{self.right[1].value.lower()}s": self.right[0]}
+            return datetime.date.today() - value >= datetime.timedelta(**time_value)
+        # TODO: check if the shotgrid implementation is strict or not
+        elif self.operator == Operator.IN_NEXT:
+            time_value = {f"{self.right[1].value.lower()}s": self.right[0]}
+            return value - datetime.date.today() >= datetime.timedelta(**time_value)
+        elif self.operator == Operator.IS:
+            return value == self.right
+        elif self.operator == Operator.IS_NOT:
+            return value != self.right
+        # TODO: check if the shotgrid implementation is strict or not
+        elif self.operator == Operator.LESS_THAN:
+            return value <= self.right
+        # TODO: check if the shotgrid implementation is strict or not
+        elif self.operator == Operator.NOT_BETWEEN:
+            return value < self.right[0] or value > self.right[1]
+        elif self.operator == Operator.NOT_CONTAINS:
+            return self.right not in value
+        elif self.operator == Operator.NOT_IN:
+            return value not in self.right
+        # TODO: check if the shotgrid implementation is strict or not
+        elif self.operator == Operator.NOT_IN_LAST:
+            time_value = {f"{self.right[1].value.lower()}s": self.right[0]}
+            return datetime.date.today() - value < datetime.timedelta(**time_value)
+        # TODO: check if the shotgrid implementation is strict or not
+        elif self.operator == Operator.NOT_IN_NEXT:
+            time_value = {f"{self.right[1].value.lower()}s": self.right[0]}
+            return value - datetime.date.today() < datetime.timedelta(**time_value)
+        elif self.operator == Operator.STARTS_WITH:
+            return value.startswith(self.right)
+        elif self.operator == Operator.TYPE_IS:
+            return value.__sg_type__ == self.right
+        elif self.operator == Operator.TYPE_IS_NOT:
+            return value.__sg_type__ != self.right
